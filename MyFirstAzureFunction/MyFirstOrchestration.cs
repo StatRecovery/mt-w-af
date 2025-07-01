c506e5784b38c92fa18c6d718c7b25304875268c
Shawn Neckelmann <shawn@statrecovery.com>
2025-07-01 11:48:57 -0500

    gitignore

--- MyFirstAzureFunction/MyFirstOrchestration.cs
+++ MyFirstAzureFunction/MyFirstOrchestration.cs
@@ -1,30 +1,22 @@
-﻿// MyFirstOrchestration.cs
-
-using Microsoft.Azure.Functions.Worker; // For FunctionName
-using Microsoft.DurableTask; // For TaskOrchestrationContext
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
 using Microsoft.Extensions.Logging;
 
 namespace MyFirstAzureFunction
 {
-    internal class MyFirstOrchestration(ILogger<MyFirstOrchestration> classLogger)
+    internal class MyFirstOrchestration
     {
         [Function(nameof(TheOrchestration))]
         public async Task<string> TheOrchestration(
             [OrchestrationTrigger] TaskOrchestrationContext context,
-            Welcome welcome) // Directly bind the Welcome message as input
+            Welcome welcome)
         {
             var logger = context.CreateReplaySafeLogger(nameof(TheOrchestration));
-            // Updated logging to reflect Welcome.Name only
             logger.LogInformation($"Orchestration '{context.InstanceId}' started with Name: {welcome.Name}");
-
-            // Removed calls to ProcessMessageActivity as MessageContent is no longer in Welcome
 
             logger.LogInformation($"Logging critical message for: {welcome.Name}");
 
             return await Task.FromResult(welcome.Name);
         }
-
-        // Removed Activity Functions as they are no longer called by the orchestration
-        // If you need activity functions, you would re-add them here and call them from TheOrchestration.
     }
 }
