b32b91033c417a4d7941dad9013f5fb91e8c1656
Shawn Neckelmann <shawn@statrecovery.com>
2025-07-01 11:49:17 -0500

    Work i progress...

--- MyFirstAzureFunction/MyFirstOrchestration.cs
+++ MyFirstAzureFunction/MyFirstOrchestration.cs
@@ -2,21 +2,20 @@
 using Microsoft.DurableTask;
 using Microsoft.Extensions.Logging;
 
-namespace MyFirstAzureFunction
+namespace MyFirstAzureFunction;
+
+internal class MyFirstOrchestration
 {
-    internal class MyFirstOrchestration
+    [Function(nameof(TheOrchestration))]
+    public async Task<string> TheOrchestration(
+        [OrchestrationTrigger] TaskOrchestrationContext context,
+        Welcome welcome)
     {
-        [Function(nameof(TheOrchestration))]
-        public async Task<string> TheOrchestration(
-            [OrchestrationTrigger] TaskOrchestrationContext context,
-            Welcome welcome)
-        {
-            var logger = context.CreateReplaySafeLogger(nameof(TheOrchestration));
-            logger.LogInformation($"Orchestration '{context.InstanceId}' started with Name: {welcome.Name}");
+        var logger = context.CreateReplaySafeLogger(nameof(TheOrchestration));
+        logger.LogInformation($"Orchestration '{context.InstanceId}' started with Name: {welcome.Name}");
 
-            logger.LogInformation($"Logging critical message for: {welcome.Name}");
+        logger.LogInformation($"Logging critical message for: {welcome.Name}");
 
-            return await Task.FromResult(welcome.Name);
-        }
+        return await Task.FromResult(welcome.Name);
     }
 }
